617bc5f70b32b0048b3f4b3e4c2077d73de461f0
Эмиль Алалаев <eaalalaev@edu.hse.ru>
2019-06-14 18:57:22 +0500

    проверил решение на сайте все работает

--- SecondNumber/Program.cs
+++ SecondNumber/Program.cs
@@ -179,6 +179,7 @@ namespace Packets
         static List<string> CreateData(string ServerName, int Send, int Received, int Minimum, int Maximum, int Average)
         {
             // Возвращаемый список
+
             List<string> result = new List<string>();
             // если все пакеты утеряны
             if (Received == 0)
